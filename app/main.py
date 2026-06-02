4e4ca6861b97b8bc5b2c0f3624b8e4ae5f98bce4
nagasai-2003 <nagasai2003apr19@gmail.com>
2026-06-02 12:00:56 +0530

    Enable CORS in backend & Add customizable FastAPI Backend URL in Settings UI

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI(title="AI Career GPS Backend", version="1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
